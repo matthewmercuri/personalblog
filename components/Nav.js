1dfacdb4acbc86bb19cc5a2c8f351e66bf841b7b
Matthew Mercuri <mercurimatthew@gmail.com>
2021-05-13 00:54:11 -0400

    finished navbar and early cleanup

---
+++ components/Nav.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import Link from 'next/link'
+import styles from '../styles/Nav.module.css'
+
+export default function Nav () {
+  const [click, setClick] = useState(false)
+  const handleClick = () => setClick(!click)
+  const closeMobileMenu = () => setClick(false)
+
+  return (
+    <nav className={styles.mainNav}>
+      <div className={styles.regNav}>
+        <img className={styles.logo} src='/baselogo.svg' />
+        <ul>
+          <li><Link href='/'><a>home</a></Link></li>
+          <li><Link href='/blog'><a>blog</a></Link></li>
+          <li><Link href='/about'><a>about</a></Link></li>
+          <li><Link href='/contact'><a>contact</a></Link></li>
+        </ul>
+      </div>
+      <div className={click ? `${styles.mobileNavOpen} ${styles.mobileNav}` : styles.mobileNav}>
+        <div className={styles.topMobileNav}>
+          <img className={styles.mobileLogo} src='/baselogoinv.svg' />
+          <img className={click ? styles.menuOpen : styles.menu} onClick={handleClick} src='/menu.svg' />
+          <img className={click ? styles.close : styles.hideClose} onClick={closeMobileMenu} src='/x.svg' />
+        </div>
+        <ul className={click ? styles.linksOpen : styles.linksClosed}>
+          <li><Link href='/'><a onClick={closeMobileMenu}>home</a></Link></li>
+          <li><Link href='/blog'><a onClick={closeMobileMenu}>blog</a></Link></li>
+          <li><Link href='/about'><a onClick={closeMobileMenu}>about</a></Link></li>
+          <li><Link href='/contact'><a onClick={closeMobileMenu}>contact</a></Link></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
