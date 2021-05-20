b849ff0d43594abe2652d8480da35ddf527f496a
Matthew Mercuri <mercurimatthew@gmail.com>
2021-05-20 13:26:52 -0400

    cleaned up to prepare contentful deprecation

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import { fetchEntries } from '../../contentful/setup'
-import BlogPosts from '../../components/BlogPosts'
 
 export default function blog ({ posts }) {
   return (
@@ -9,23 +7,6 @@ export default function blog ({ posts }) {
         <title>blog | matthew mercuri</title>
       </Head>
       <h1>Blog</h1>
-      <BlogPosts posts={posts} />
     </div>
   )
-}
-
-export async function getStaticProps ({ params }) {
-  const allPosts = await fetchEntries()
-
-  if (!allPosts) {
-    return {
-      notFound: true
-    }
-  }
-
-  return {
-    props: {
-      posts: allPosts
-    }
-  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 import Welcome from '../components/Welcome'
-import Recent from '../components/Recent'
-import { client } from '../contentful/setup'
 
 export default function Home ({ posts }) {
   return (
@@ -10,40 +8,6 @@ export default function Home ({ posts }) {
         <title>matthew mercuri</title>
       </Head>
       <Welcome />
-      <Recent posts={posts} />
     </>
   )
 }
-
-export async function getStaticProps ({ params }) {
-  const query = { limit: 5, content_type: 'blogPost' }
-  const recentPosts = await client.getEntries(query)
-
-  if (!recentPosts) {
-    return {
-      notFound: true
-    }
-  }
-
-  return {
-    props: {
-      posts: recentPosts.items
-    }
-  }
-}
-
-// export async function getStaticProps ({ params }) {
-//   const allPosts = await fetchEntries()
-
-//   if (!allPosts) {
-//     return {
-//       notFound: true
-//     }
-//   }
-
-//   return {
-//     props: {
-//       posts: allPosts
-//     }
-//   }
-// }
