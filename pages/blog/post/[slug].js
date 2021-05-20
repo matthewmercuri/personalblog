adf31c69c9f32bc119baea98c64bf47ebcad26e4
Matthew Mercuri <mercurimatthew@gmail.com>
2021-05-20 14:09:48 -0400

    deprecated contentful

--- pages/blog/post/[slug].js
+++ pages/blog/post/[slug].js
@@ -1,6 +1,60 @@
-export default function Posts() {
-    return (
-        <div>
-        </div>
-    )
+import fs from 'fs'
+import matter from 'gray-matter'
+import { MDXRemote } from 'next-mdx-remote'
+import { serialize } from 'next-mdx-remote/serialize'
+import dynamic from 'next/dynamic'
+import Head from 'next/head'
+import Link from 'next/link'
+import path from 'path'
+import { postFilePaths, POSTS_PATH } from '../../../utils/mdxUtils'
+
+export default function PostPage({ source, frontMatter }) {
+  return (
+    <>
+    <Link href="/">
+      <a>👈 Go back home</a>
+    </Link>
+    <div>
+      <h1>{frontMatter.title}</h1>
+      <p>{frontMatter.description}</p>
+    </div>
+    <main>
+      <MDXRemote {...source} />
+    </main>
+    </>
+  )
+}
+
+export const getStaticProps = async ({ params }) => {
+  const postFilePath = path.join(POSTS_PATH, `${params.slug}.mdx`)
+  const source = fs.readFileSync(postFilePath)
+
+  const { content, data } = matter(source)
+
+  const mdxSource = await serialize(content, {
+    // Optionally pass remark/rehype plugins
+    mdxOptions: {
+      remarkPlugins: [],
+      rehypePlugins: [],
+    },
+    scope: data,
+  })
+
+  return {
+    props: {
+      source: mdxSource,
+      frontMatter: data,
+    },
+  }
+}
+
+export const getStaticPaths = async () => {
+  const paths = postFilePaths
+    .map((path) => path.replace(/\.mdx?$/, ''))
+    .map((slug) => ({ params: { slug } }))
+
+  return {
+    paths,
+    fallback: false,
+  }
 }
