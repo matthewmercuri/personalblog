4ce11443c6319c062153b30aa45c16a3a26f5131
Matthew Mercuri <mercurimatthew@gmail.com>
2021-05-23 00:46:02 -0400

    fixing blog post functionality

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,7 +6,7 @@ export default function ContactForm () {
       <h4>Please fill out this form if you have any questions, suggestions, or business inquiries.</h4>
       <form name='contactForm' method='POST' encType='application/x-www-form-urlencoded' data-netlify='true' data-netlify-recaptcha='true' netlify-honeypot='bot-field'>
         <input type='hidden' name='form-name' value='contactForm' />
-        <p class={styles.hidden}>
+        <p className={styles.hidden}>
           <label>Don’t fill this out if you’re human: <input name='bot-field' /></label>
         </p>
         <p>
--- components/Post.js
+++ components/Post.js
@@ -1,10 +1,17 @@
 import { MDXRemote } from 'next-mdx-remote'
+import Image from 'next/image'
+import customImage from './customImage'
+import Link from 'next/link'
+
+import styles from './blogstyles/Post.module.css'
+
+const components = { Image, Link, customImage }
 
 export default function Post ({ source, frontMatter }) {
   return (
-    <div>
+    <div className={styles.post}>
       <h1>{frontMatter.title}</h1>
-      <MDXRemote {...source} />
+      <MDXRemote {...source} components={components} />
     </div>
   )
 }
