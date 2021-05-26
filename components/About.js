d920703f26bddbbf228f57cab14fabb10d371d19
Matthew Mercuri <mercurimatthew@gmail.com>
2021-05-26 17:56:06 -0400

    added blog card css updated and other css changes

--- components/About.js
+++ components/About.js
@@ -4,6 +4,7 @@ import styles from '../styles/About.module.css'
 export default function About () {
   return (
     <div className={styles.about}>
+      <img className={styles.quot1} src='/accents/quotation.svg' alt='accent' />
       <p><span>Finance, mathematics, sports, and programming.</span> I like working with them all. My name is Matthew and I appreciate you spending some time on my personal website. It is
         here that I will showcase some of my projects, research, and learning.
       </p>
@@ -15,8 +16,13 @@ export default function About () {
       </p>
       <p>I have an Honours Bachelor of Science (HBSc) in financial mathematics and I enjoy any opportunity to employ my programming and mathematics knowledge to do some fun things.</p>
       <p>If you have any suggestions or inquiries please feel free to get in touch with me through my <Link href='/contact'><a><span>contact</span></a></Link> form.</p>
-      <a href='https://www.linkedin.com/in/matthew-mercuri/'><img src='/social/linkedinlogo.svg' alt='link to linkedin' /></a>
-      <a href='https://github.com/matthewmercuri'><img src='/social/githublogo.svg' alt='link to github' /></a>
+      <img className={styles.quot2} src='/accents/quotation.svg' alt='accent' />
+      <div className={styles.sig}>
+        <h3>Matthew Mercuri</h3>
+        <div><a href='https://www.linkedin.com/in/matthew-mercuri/'><img className={styles.aboutSocial} src='/social/linkedinlogo.svg' alt='link to linkedin' /></a>
+          <a href='https://github.com/matthewmercuri'><img className={styles.aboutSocial} src='/social/githublogo.svg' alt='link to github' /></a>
+        </div>
+      </div>
       <p className={styles.disclaimer}>Please note that anything I share here should not be considered financial advice. Should you require advice, speak to
         an accredited financial professional.
       </p>
--- components/BlogCard.js
+++ components/BlogCard.js
@@ -4,7 +4,15 @@ import styles from './blogstyles/BlogCard.module.css'
 export default function BlogCard ({ path, metadata }) {
   return (
     <div className={styles.blogcard}>
-      <Link href={`/blog/post/${metadata.slug}`}><a><h2>{metadata.title}</h2></a></Link>
+      <img src={`/featimgs/${metadata.featureImageName}`} alt='blog post featured image' />
+      <div className={styles.info}>
+        <div className={styles.blogAccent}>
+          <p>{metadata.category}</p>
+          <div className={styles.accent} />
+        </div>
+        <Link href={`/blog/post/${metadata.slug}`}><a><h1>{metadata.title}</h1></a></Link>
+        <Link href={`/blog/post/${metadata.slug}`}><div className={styles.readMore}>Read</div></Link>
+      </div>
     </div>
   )
 }
