47cffed92d1c5ca0469cd78859553fdb42fe6392
Matthew Mercuri <mercurimatthew@gmail.com>
2021-05-30 12:27:15 -0400

    added honeypot to form

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -4,7 +4,10 @@ export default function ContactForm () {
   return (
     <div className={styles.contactForm}>
       <h4>Please fill out this form if you have any questions, suggestions, or business inquiries.</h4>
-      <form name='contact' action='/success' method='POST' data-netlify='true'>
+      <form name='contact' action='/success' method='POST' netlify-honeypot='bot-field' data-netlify='true'>
+        <p class={styles.hidden}>
+          <label>Don’t fill this out if you’re human: <input name='bot-field' /></label>
+        </p>
         <input type='hidden' name='form-name' value='contact' />
         <p>
           <label>Your Name: <input type='text' id='YourName' name='senderName' placeholder='name' maxLength='35' required /></label>
