1e07148b9595b67582898f2ef8b7b851288e7b4e
Matthew Mercuri <mercurimatthew@gmail.com>
2021-05-30 12:04:01 -0400

    tried removing captcha from form

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -4,7 +4,7 @@ export default function ContactForm () {
   return (
     <div className={styles.contactForm}>
       <h4>Please fill out this form if you have any questions, suggestions, or business inquiries.</h4>
-      <form name='contact' action='/success' method='POST' data-netlify='true' data-netlify-recaptcha='true' netlify-honeypot='bot-field'>
+      <form name='contact' action='/success' method='POST' data-netlify='true' netlify-honeypot='bot-field'>
         <input type='hidden' name='form-name' value='contact' />
         <p className={styles.hidden}>
           <label>Don’t fill this out if you’re human: <input name='bot-field' /></label>
@@ -18,7 +18,7 @@ export default function ContactForm () {
         <p>
           <label>Message: <textarea id='message' name='message' placeholder='type your message here...' maxLength='750' required /></label>
         </p>
-        <div data-netlify-recaptcha='true' />
+        {/* <div data-netlify-recaptcha='true' /> */}
         <p>
           <button type='submit'>Send</button>
         </p>
