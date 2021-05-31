6bae4192003a8b7011b1a92424faf58df078193e
Matthew Mercuri <mercurimatthew@gmail.com>
2021-05-31 16:47:15 -0400

    fixed feature image card background colour issue

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -2,8 +2,19 @@ import Link from 'next/link'
 import styles from './blogstyles/BlogCard.module.css'
 
 export default function BlogCard ({ path, metadata }) {
+  const cat = metadata.category
+  let bgclass
+
+  if (cat === 'Quantitative Finance') {
+    bgclass = styles.qf
+  } else if (cat === 'Data Science') {
+    bgclass = styles.ds
+  } else {
+    bgclass = styles.default
+  }
+
   return (
-    <div className={styles.blogcard}>
+    <div className={`${styles.blogcard} ${bgclass}`}>
       <Link href={`/blog/post/${metadata.slug}`}><img src={`/featimgs/${metadata.featureImageName}`} alt='blog post featured image' /></Link>
       <div className={styles.info}>
         <div className={styles.blogAccent}>
